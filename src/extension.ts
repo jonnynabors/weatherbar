06b62c19bee520f25c53f1cd23db2612a965face
Jonny Nabors <jonnynabors@gmail.com>
2019-06-26 21:05:17 -0400

    Fetch weather on interval if user has configured

--- src/extension.ts
+++ src/extension.ts
@@ -17,32 +17,54 @@ export function activate(context: vscode.ExtensionContext) {
     async () => {
       const zipCode = vscode.workspace
         .getConfiguration("weatherBar")
-        .get("zipCode");
+        .get("zipCode") as number;
+
       // add validation to ensure user has configured a 5 character ZIP
-      if (typeof zipCode !== "number" || zipCode.toString().length !== 5) {
+      if (zipCode.toString().length !== 5) {
         vscode.window.showErrorMessage(
           `WeatherBar: The ZIP Code ${zipCode} is invalid.`
         );
         return;
       }
 
-      try {
-        const currentWeather = (await getWeather(zipCode)) as [WeatherData];
-        //   @ts-ignore
-        statusBarItem.text = `${Weather[currentWeather[0].main]} ${
-          currentWeather[0].description
-        }`;
-      } catch (error) {
-        console.error("Error occurred while fetching weather for zip", zipCode);
-        // TODO: HANDLE ERRORS
-      }
+      await showWeatherAsEmoji(zipCode, statusBarItem);
 
-      statusBarItem.show();
+      const intervalInMinutes = vscode.workspace
+        .getConfiguration("weatherBar")
+        .get("intervalInMinutes") as number;
+
+      if (intervalInMinutes !== undefined) {
+        // user has set an interval
+        setInterval(async () => {
+          await showWeatherAsEmoji(zipCode, statusBarItem);
+        }, intervalInMinutes * 60000);
+
+        // DO SOME MINUTES TO SECONDS FOO
+      }
+      // if interval is set, check weather on that interval
+      // define minimum interval
     }
   );
 
   vscode.commands.executeCommand("extension.weatherBar");
   context.subscriptions.push(disposable);
+}
+
+async function showWeatherAsEmoji(
+  zipCode: number,
+  statusBarItem: vscode.StatusBarItem
+) {
+  try {
+    const currentWeather = (await getWeather(zipCode)) as [WeatherData];
+    //   @ts-ignore
+    statusBarItem.text = `${Weather[currentWeather[0].main]} ${
+      currentWeather[0].description
+    }`;
+  } catch (error) {
+    console.error("Error occurred while fetching weather for zip", zipCode);
+    // TODO: HANDLE ERRORS
+  }
+  statusBarItem.show();
 }
 
 // this method is called when your extension is deactivated
